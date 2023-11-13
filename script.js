c7a7568b39ff3e23ba6385feef6ee2a21e8a445d
Zhekastorm <81801950+Zhekastorm@users.noreply.github.com>
2023-11-13 09:11:33 +0300

    Версия 2

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-let word_1 = '123';
+$(function (){
+    var header = $("#header")
+    var scrollPrev = 0
 
-console.log(word_1)
+    $(window).scroll(function (){
+        var scrolled = $(window).scrollTop()
+
+        if (scrolled > 100 && scrolled > scrollPrev){
+            header.addClass('hidden')
+        } else {
+            header.removeClass('hidden')
+        }
+        scrollPrev = scrolled;
+    });
+    
+    // $("[data-scroll]").on("click", function(event) {
+    //     event.preventDefault();
+
+    //     var elemId = $(this).data('scroll');
+    //     var elemOffset = $(elemId).offset().top;
+
+
+    //     $("html, body").animate({
+    //         scrollTop: elemOffset - 50
+    //     }, 1000);
+    // });
+
+    $("[data-collapse]").on("click", function (event){
+        event.preventDefault();
+
+
+        var $this = $(this),
+            elemId = $this.data('collapse');
+
+        $this.toggleClass("active")
+
+    });
+
+
+    var reviews__slider = $(".reviews__slider")
+    reviews__slider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2500,
+        dots: true,
+        arrows: false,
+    });
+
+})
+
+
