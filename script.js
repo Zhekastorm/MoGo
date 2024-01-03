ebd6d6df71e843f63f1d5be7f43efb336200c9dd
Zhekastorm <81801950+Zhekastorm@users.noreply.github.com>
2024-01-03 18:59:26 +0300

    Версия 3

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(function (){
     var nav__link = $(".nav__link")
     var scrollPrev = 0
 
+
+    // Header__fixed
     $(window).scroll(function (){
         var scrolled = $(window).scrollTop()
 
@@ -15,20 +17,20 @@ $(function (){
     });
 
 
-    
+    // Scroll
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
 
         var elemId = $(this).data('scroll');
         var elemOffset = $(elemId).offset().top;
-
+        
         document.getElementById("check__menu").checked = false;
+        header.removeClass('hidden');
 
         $("html, body").animate({
             scrollTop: elemOffset - 45
         }, 3000);
-
-
+        
     });
 
     $("[scroll]").on("click", function (event){
@@ -39,6 +41,8 @@ $(function (){
         }, 3000);
     });
 
+
+    // Accordion
     $("[data-collapse]").on("click", function (event){
         event.preventDefault();
 
@@ -51,6 +55,7 @@ $(function (){
     });
 
 
+    // Slider
     var reviews__slider = $(".reviews__slider")
     reviews__slider.slick({
         slidesToShow: 1,
@@ -59,6 +64,27 @@ $(function (){
         speed: 2500,
         dots: true,
         arrows: false,
+    });
+
+
+    //  Active__links
+    window.addEventListener('scroll', function (){
+        var scrollH = window.scrollY;
+
+        document.querySelectorAll('.section').forEach((el, i) =>{
+            if (el.offsetTop - document.querySelector(".main__menu").clientHeight <= scrollH){
+                document.querySelectorAll(".main__menu a").forEach((el) =>{
+                    if (el.classList.contains('active')){
+                        el.classList.remove('active')
+                    }
+                });
+                console.log(i)
+                if (i != 0){
+                    document.querySelectorAll('a')[i].classList.add('active')
+                }
+            }
+            
+        });
     });
 
 })
